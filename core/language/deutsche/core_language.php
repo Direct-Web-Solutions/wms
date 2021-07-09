4b74fb79462bcda0f9291024d75d39c61e14d8c6
directwebsolutions <87156874+directwebsolutions@users.noreply.github.com>
2021-07-08 19:23:23 -0600

    Create core_language.php

---
+++ core/language/deutsche/core_language.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * 
+ * core/languages/deutsche/core_language.php
+ * WMS (Website Management System)
+ *
+ * @category    language
+ * @package     wms
+ * @author      Darryn Fehr
+ * @copyright   2018 Direct Web Solutions
+ * @license     https://www.directwebsolutions.ca/wms/license
+ * @version     2.0.0
+ * @release     June 5, 2021
+ * @link        https://www.directwebsolutions.ca/wms/latest
+ * @since       File available since Release 2.0.0
+ * @deprecated  File deprecated in Release 3.0.0
+ * 
+**/
+
+// General info for this language
+$langinfo['name'] = "Deutsche";
+$langinfo['htmllang'] = "de";
+$langinfo['charset'] = "UTF-8";
+
+// Language Global Variables
+$l['na'] = "N/A";
+$l['unknown'] = "Unbekannt";
+
+$l['debug_generated_in'] = "Generiert in {1}";
+$l['debug_weight'] = "({1}% PHP / {2}% {3})";
+$l['debug_sql_queries'] = "SQL Anfragen: {1}";
+$l['debug_server_load'] = "Server Laden: {1}";
+$l['debug_memory_usage'] = "Speichernutzung: {1}";
