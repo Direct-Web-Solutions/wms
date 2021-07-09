4af291f9b38789ce48353dd7eb92da7802ca893c
directwebsolutions <87156874+directwebsolutions@users.noreply.github.com>
2021-07-08 18:49:33 -0600

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 
+ * 404.php
+ * WMS (Website Management System)
+ *
+ * @category    404 File
+ * @package     wms
+ * @author      Darryn Fehr
+ * @copyright   2018 Direct Web Solutions
+ * @license     https://www.directwebsolutions.ca/wms/license
+ * @version     2.0.0
+ * @release     June 5, 2021
+ * @link        https://www.directwebsolutions.ca/wms/latest
+ * @since       File available since Release 2.0.0
+ * @deprecated  File deprecated in Release 3.0.0
+ * 
+**/
+
+define('ALLOW_ACCESS', 1);
+define('CURRENT_SCRIPT', '404.php');
+include("global.php");
+$templates->generate_error(404);
